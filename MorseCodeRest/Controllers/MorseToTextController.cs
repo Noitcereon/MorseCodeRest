ed58de447809096bbcebb527029b16eb1eb90256
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2020-10-30 14:46:44 +0100

    Add project files.

---
+++ MorseCodeRest/Controllers/MorseToTextController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MorseCodeRest.Managers;
+
+namespace MorseCodeRest.Controllers
+{
+    [ApiController]
+    [Route("api/translate/")]
+    public class MorseToTextController
+    {
+        private MorseToTextManager _manager = new MorseToTextManager();
+
+        [HttpGet("morse")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetNormalChar([FromQuery]string morseCharacter)
+        {
+            return Ok(_manager.GetNormalChar(morseCharacter));
+        }
+    }
+}
